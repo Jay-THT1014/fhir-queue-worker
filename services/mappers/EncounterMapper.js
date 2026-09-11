bda2d83f1df0f59efc8dd379880a2b64afc16442
jayanarayanan <jayanarayanan@tiatech.net>
2026-09-11 15:40:21 +0530

    Updated resources Encounter and related person.

--- services/mappers/EncounterMapper.js
+++ services/mappers/EncounterMapper.js
@@ -8,44 +8,83 @@ async function mapEncounter(data, sourceSystem, orgId, tenantId, db) {
     status: data.status || "finished",
     class: {
       system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
-      code: "AMB",
-      display: "ambulatory",
+      code: data.class_code || "AMB",
+      display: data.class_display || "ambulatory",
     },
+    type: data.type_code
+      ? [
+        {
+          coding: [
+            {
+              system: "http://snomed.info/sct",
+              code: data.type_code,
+              display: data.type_display || undefined,
+            },
+          ],
+        },
+      ]
+      : undefined,
+    reasonCode: data.reason_code
+      ? [
+        {
+          coding: [
+            {
+              system: "http://hl7.org/fhir/sid/icd-10-cm",
+              code: data.reason_code,
+              display: data.reason_display || undefined,
+            },
+          ],
+        },
+      ]
+      : undefined,
+    hospitalization: data.discharge_disposition_code
+      ? {
+        dischargeDisposition: {
+          coding: [
+            {
+              system: "http://terminology.hl7.org/CodeSystem/discharge-disposition",
+              code: data.discharge_disposition_code,
+              display: data.discharge_disposition_display || undefined,
+            },
+          ],
+        },
+      }
+      : undefined,
     period:
       data.encounterStart || data.encounterEnd
         ? {
-            start: data.encounterStart || undefined,
-            end: data.encounterEnd || undefined,
-          }
+          start: data.encounterStart || undefined,
+          end: data.encounterEnd || undefined,
+        }
         : undefined,
     location: data.locationRef
       ? [
-          {
-            location: {
-              reference: data.locationRef,
-              display: data.locationName || undefined,
-            },
-            status: "completed",
-            period:
-              data.start || data.end
-                ? {
-                    start: data.start || undefined,
-                    end: data.end || undefined,
-                  }
-                : undefined,
+        {
+          location: {
+            reference: data.locationRef,
+            display: data.locationName || undefined,
           },
-        ]
+          status: "completed",
+          period:
+            data.start || data.end
+              ? {
+                start: data.start || undefined,
+                end: data.end || undefined,
+              }
+              : undefined,
+        },
+      ]
       : undefined,
     subject: buildReference("Patient", await resolveFhirId(db, tenantId, sourceSystem, "Patient", data.patient_id)),
     participant: data.doctor_id
       ? [
-          {
-            individual: buildReference(
-              "Practitioner",
-              await resolveFhirId(db, tenantId, sourceSystem, "Practitioner", data.doctor_id),
-            ),
-          },
-        ]
+        {
+          individual: buildReference(
+            "Practitioner",
+            await resolveFhirId(db, tenantId, sourceSystem, "Practitioner", data.doctor_id),
+          ),
+        },
+      ]
       : undefined,
     serviceProvider: buildReference(
       "Organization",
--- services/mappers/RelatedPersonMapper.js
+++ services/mappers/RelatedPersonMapper.js
@@ -1,4 +1,4 @@
-const { buildIdentifier, buildReference } = require("../../utils/fhirHelpers");
+const { buildIdentifier, buildReference, buildAddress } = require("../../utils/fhirHelpers");
 const { resolveFhirId } = require("./resolver");
 
 async function mapRelatedPerson(data, sourceSystem, orgId, tenantId, db) {
@@ -10,6 +10,16 @@ async function mapRelatedPerson(data, sourceSystem, orgId, tenantId, db) {
       ? [{ coding: [{ system: "http://terminology.hl7.org/CodeSystem/v3-RoleCode", code: data.relationship_code }] }]
       : undefined,
     name: data.name ? [{ text: data.name }] : undefined,
+    telecom: buildTelecom(data.email, data.email_use, data.phone, data.phone_use),
+    address: buildAddress(
+      data.address_line1 || data.address,
+      data.address_line2,
+      data.city,
+      data.state,
+      data.postalCode || data.postalcode,
+      data.country,
+      data.address_use || data.use
+    ),
   };
 }
 
